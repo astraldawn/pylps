ab7bd8a5a0c3add4e0b9247d9d75f3fa192211d6
Mark <mark.lee.chu.yong@gmail.com>
2018-01-14 00:04:23 +0000

    Updated passing forward information into a solver goal object as well

--- pylps/solver.py
+++ pylps/solver.py
@@ -29,104 +29,83 @@ def solve_multigoal(multigoal, cycle_time):
     return G_SOLVED if solved_cnt == len(multigoal.goals) else G_UNSOLVED
 
 
-def solve_goal(goal, cur_subs, cycle_time):
+def solve_goal(goal, subs, cycle_time):
     # requirements = set()
 
     # Check if this goal is associated with some clause
     # This will match the first clause available
     cycle_temporal_sub_used = False
-    goal_temporal_vars = None
 
-    try:
-        goal_object_original = goal[0]
-        goal_temporal_vars = tuple(
-            var(temporal_var.name)
-            for temporal_var in goal[1:]
-        )
-
-        # Make a check if there is a temporal substitution from first step
-        for temporal_var in goal[1:]:
-            if var(temporal_var.name) in cur_subs:
-                cycle_temporal_sub_used = True
-    except TypeError:
-        goal_object_original = goal
-
-    goal_object = copy.deepcopy(goal_object_original)
     # print(goal, subs, cycle_time, temporal, cycle_temporal_sub_used)
 
-    ret = SolverGoal(
-        goal=goal_object,
-        cur_subs=cur_subs,
-        goal_temporal=goal_temporal_vars
+    solver_goal = SolverGoal(
+        goal=copy.deepcopy(goal),
+        subs=copy.deepcopy(subs)
     )
 
-    KB_clauses = KB.get_clauses(goal_object_original)
+    if solver_goal.temporal_vars:
+        for temporal_var in solver_goal.temporal_vars:
+            if temporal_var in subs:
+                cycle_temporal_sub_used = True
+
+    KB_clauses = KB.get_clauses(solver_goal.obj)
 
     if CONFIG.single_clause:
         KB_clauses = KB_clauses[:1]
 
     if KB_clauses:
         for clause in KB_clauses:
-            clause_res = solve_goal_complex(
+            solver_goal = solve_goal_complex(
                 clause,
-                goal_object,
-                goal_temporal_vars,
-                cur_subs,
+                solver_goal,
                 cycle_temporal_sub_used,
                 cycle_time
             )
 
-            if clause_res.result is G_CLAUSE_FAIL:
+            if solver_goal.result is G_CLAUSE_FAIL:
                 continue
 
-            if clause_res.result is G_SOLVED or clause_res.result is G_DISCARD:
-                ret.update_result(clause_res.result)
-                return ret
+            if (solver_goal.result is G_SOLVED or
+                    solver_goal.result is G_DISCARD):
+                return solver_goal
     else:
-        clause_res = solve_goal_single(
-            goal,
-            cur_subs,
-            cycle_time
-        )
+        solver_goal = solve_goal_single(solver_goal, cycle_time)
 
-        print(clause_res, new_subs)
+        print(solver_goal)
 
-    ret.clear_subs()
-    ret.update_result(G_UNSOLVED)
-    return ret
+    solver_goal.clear_subs()
+    return solver_goal
 
 
-def solve_goal_complex(clause, goal_object, goal_temporal_vars, cur_subs,
-                       cycle_temporal_sub_used, cycle_time):
+def solve_goal_complex(clause, goal, cycle_temporal_sub_used, cycle_time):
 
     clause_goal = clause.goal[0]
     clause_goal_object = clause_goal[0]  # This may cause issues
-    subs = copy.deepcopy(cur_subs)  # Create a new subs object
-
-    ret = SolverGoal(
-        goal=goal_object,
-        cur_subs=subs,
-        goal_temporal=goal_temporal_vars,
-        result=G_UNSOLVED
-    )
 
     # TODO: FIX / CHANGE THIS
     # Check if the objects match
-    if clause_goal_object != goal_object:
-        return ret
+    if clause_goal_object != goal.obj:
+        return goal
 
     for req in clause.reqs:
-        single_res = solve_goal_single(req, subs, cycle_time)
+        req_goal = SolverGoal(
+            goal=req,
+            subs=copy.deepcopy(goal.subs)
+        )
 
-        if (single_res.result is G_DISCARD or
-                single_res.result is G_CLAUSE_FAIL):
-            return single_res
+        req_goal = solve_goal_single(req_goal, cycle_time)
 
-        if single_res.new_subs:
-            ret.update_subs(single_res.new_subs)
+        if (req_goal.result is G_DISCARD or
+                req_goal.result is G_CLAUSE_FAIL):
+            goal.update_result(req_goal.result)
+            goal.clear_subs()
+            return goal
+
+        if req_goal.new_subs:
+            goal.update_subs(req_goal.new_subs)
 
     # Check if can meet all the temporal reqs for clause
-    combined_subs = {**ret.cur_subs, **ret.new_subs}
+    combined_subs = {**goal.subs, **goal.new_subs}
     temporal_satisfied_cnt = 0
     for temporal_var in clause_goal[1:]:
         if var(temporal_var.name) in combined_subs:
@@ -135,85 +114,65 @@ def solve_goal_complex(clause, goal_object, goal_temporal_vars, cur_subs,
     temporal_satisfied = (temporal_satisfied_cnt == len(clause_goal[1:]))
 
     if temporal_satisfied:
-        goal_temporal_vars = reify(goal_temporal_vars, combined_subs)
+        goal_temporal_vars = reify(goal.temporal_vars, combined_subs)
 
-        if not strictly_increasing(goal_temporal_vars):
-            ret.clear_subs()
-            ret.update_result(G_DISCARD)
-            return ret
+        if strictly_increasing(goal_temporal_vars):
+            goal.update_result(G_SOLVED)
+            goal.temporal_vars = goal_temporal_vars
+            return goal
 
-        ret.update_result(G_SOLVED)
-        return ret
+        goal.clear_subs()
+        goal.update_result(G_DISCARD)
+        return goal
 
-    return ret
+    return goal
 
 
-def solve_goal_single(req, subs, cycle_time):
+def solve_goal_single(goal, cycle_time):
 
-    new_subs = copy.deepcopy(subs)
-    req_temporal_vars = None
-    req_temporal_satisfied = False
+    reify_goal = None
+    goal_temporal_satisfied = True
 
-    try:
-        req_object = req[0]
-        req_temporal_vars = tuple(
-            var(temporal_var.name)
-            for temporal_var in req[1:]
-        )
-    except TypeError:
-        req_temporal_satisfied = True
-        req_object = req
-
-    ret = SolverGoal(
-        goal=req_object,
-        cur_subs=copy.deepcopy(subs),
-        goal_temporal=req_temporal_vars
-    )
-
-    reify_req = None
-
-    if req_temporal_vars:
-        reify_req = reify(req_temporal_vars, subs)
-        if isinstance(reify_req[0], int):
-            reify_req = (reify_req[0], reify_req[0] + 1)
+    if goal.temporal_vars:
+        reify_goal = reify(goal.temporal_vars, goal.subs)
+        if isinstance(reify_goal[0], int):
+            reify_goal = (reify_goal[0], reify_goal[0] + 1)
 
             # This should be reviewed if fails
-            ret.update_subs(unify(req_temporal_vars, reify_req))
+            goal.update_subs(unify(goal.temporal_vars, reify_goal))
             # new_subs.update(unify(req_temporal_vars, reify_req))
 
-            reify_valid = reify_req[0] == cycle_time
+            reify_valid = reify_goal[0] == cycle_time
 
             if not reify_valid:
-                ret.clear_subs()
-                ret.update_result(G_DISCARD)
-                return ret
+                goal.clear_subs()
+                goal.update_result(G_DISCARD)
+                return goal
         else:
-            raise UnhandledOutcomeError(reify_req)
+            raise UnhandledOutcomeError(reify_goal)
 
-        req_temporal_satisfied_cnt = 0
-        for item in reify_req:
+        goal_temporal_satisfied_cnt = 0
+        for item in reify_goal:
             if isinstance(item, int):
-                req_temporal_satisfied_cnt += 1
+                goal_temporal_satisfied_cnt += 1
 
-        req_temporal_satisfied = (
-            req_temporal_satisfied_cnt == len(req_temporal_vars))
+        goal_temporal_satisfied = (
+            goal_temporal_satisfied_cnt == len(goal.temporal_vars))
 
-    # print(req_temporal_satisfied)
-
-    if req_temporal_satisfied:
-        combined_subs = {**ret.cur_subs, **ret.new_subs}
-        req_temporal_vars = reify(req_temporal_vars, combined_subs)
-        if req_object.BaseClass is ACTION:
+    if goal_temporal_satisfied:
+        combined_subs = {**goal.subs, **goal.new_subs}
+        goal_temporal_vars = reify(goal.temporal_vars, combined_subs)
+        if goal.obj.BaseClass is ACTION:
             # Unify with the KB (but for now is a simple check)
             # Goal cannot be solved, discard
-            if not constraints_satisfied(req_object):
-                ret.clear_subs()
-                ret.update_result(G_CLAUSE_FAIL)
-                return ret
+            if not constraints_satisfied(goal.obj):
+                goal.clear_subs()
+                goal.update_result(G_CLAUSE_FAIL)
+                return goal
 
-            KB.log_action(req_object, req_temporal_vars)
+            KB.log_action(goal.obj, goal_temporal_vars)
 
-            causalities = KB.exists_causality(req_object)
+            causalities = KB.exists_causality(goal.obj)
 
             if causalities:
                 for outcome in causalities.outcomes:
@@ -221,7 +180,7 @@ def solve_goal_single(req, subs, cycle_time):
                         KB.remove_fluent(outcome[1])
                         KB.log_fluent(
                             outcome[1],
-                            max(req_temporal_vars),
+                            max(goal_temporal_vars),
                             F_TERMINATE
                         )
                     elif outcome[0] == A_INITIATE:
@@ -229,23 +188,25 @@ def solve_goal_single(req, subs, cycle_time):
                     else:
                         raise(UnknownOutcomeError(outcome[0]))
 
-                ret.update_result(G_SINGLE_SOLVED)
-                return ret
+                goal.update_result(G_SINGLE_SOLVED)
+                return goal
             else:
                 pass
                 # raise UnhandledObjectError("ERROR")
-        elif req_object.BaseClass is FACT:
-            unify_fact_res = unify_fact(req_object)
+        elif goal.obj.BaseClass is FACT:
+            unify_fact_res = unify_fact(goal.obj)
             if unify_fact_res:
-                print(unify_fact_res)
-                return G_SINGLE_SOLVED, new_subs
+                goal.update_subs(unify_fact_res[0])
+                goal.set_new_subs_options(unify_fact_res[1:])
+                goal.update_result(G_SINGLE_SOLVED)
+                return goal
 
-            ret.clear_subs()
-            ret.update_result(G_DISCARD)
-            return ret
+            goal.clear_subs()
+            goal.update_result(G_DISCARD)
+            return goal
         else:
-            raise UnhandledObjectError(req_object.BaseClass)
+            raise UnhandledObjectError(goal.obj.BaseClass)
 
-    ret.clear_subs()
-    ret.update_result(G_UNSOLVED)
-    return ret
+    goal.clear_subs()
+    goal.update_result(G_UNSOLVED)
+    return goal
--- pylps/solver_objects.py
+++ pylps/solver_objects.py
@@ -1,28 +1,50 @@
+from unification import *
+
 from pylps.constants import *
 
 
 class SolverGoal(object):
     BaseClass = SOLVER_GOAL
 
-    def __init__(self, goal, cur_subs=None,
-                 goal_temporal=None, result=G_NPROCESSED):
+    def __init__(self, goal, subs=None, result=G_NPROCESSED):
         self._goal = goal
-        self._cur_subs = cur_subs
-        self._goal_temporal = goal_temporal
+
+        try:
+            self._obj = goal[0]
+            self._temporal_vars = tuple(
+                var(temporal_var.name)
+                for temporal_var in goal[1:]
+            )
+        except TypeError:
+            self._obj = goal
+            self._temporal_vars = None
+
+        self._subs = subs
         self._result = result
         self._new_subs = {}
+        self._new_subs_options = []
 
     def __repr__(self):
         ret = "SolverGoal\n"
-        ret += "Goal: %s\n" % (self._goal)
+        ret += "Goal object: %s\n" % (self._obj)
+        ret += "Goal temporal vars: %s\n" % (self._temporal_vars)
         ret += "Result: %s\n" % (self._result)
-        ret += "Cur subs: %s\n" % (self._cur_subs)
+        ret += "Cur subs: %s\n" % (self._subs)
         ret += "New subs: %s\n" % (self._new_subs)
+        ret += "New subs options: %s\n" % (self._new_subs_options)
         return ret
 
     @property
-    def goal(self):
-        return self.goal
+    def obj(self):
+        return self._obj
+
+    @property
+    def temporal_vars(self):
+        return self._temporal_vars
+
+    @temporal_vars.setter
+    def temporal_vars(self, temporal_vars):
+        self._temporal_vars = temporal_vars
 
     @property
     def result(self):
@@ -32,8 +54,8 @@ class SolverGoal(object):
         self._result = result
 
     @property
-    def cur_subs(self):
-        return self._cur_subs
+    def subs(self):
+        return self._subs
 
     @property
     def new_subs(self):
@@ -44,3 +66,10 @@ class SolverGoal(object):
 
     def update_subs(self, subs):
         self._new_subs.update(subs)
+
+    @property
+    def new_subs_options(self):
+        return self._new_subs_options
+
+    def set_new_subs_options(self, subs):
+        self._new_subs_options = subs
