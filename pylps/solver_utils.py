76db306261807880a28f13709c3e74129417e2c6
Mark <mark.lee.chu.yong@gmail.com>
2018-02-17 01:14:47 +0000

    shift action / fluent processing down to reactive rule level

--- pylps/solver_utils.py
+++ pylps/solver_utils.py
@@ -26,49 +26,53 @@ def process_solutions(solutions, cycle_time):
                 chosen_solution = solution
                 max_actions = len(actions)
 
-    for action in chosen_solution[0].actions:
-        KB.log_action_new(action)
+    # for action in chosen_solution[0].actions:
+    #     KB.log_action_new(action)
 
-        if CONFIG.cycle_fluents:
-            continue
+    #     if CONFIG.cycle_fluents:
+    #         continue
 
-        causaulties = KB.exists_causality(action)
+    #     causaulties = KB.exists_causality(action)
 
-        if causaulties:
-            for causality_outcome in causaulties.outcomes:
-                outcome = causality_outcome.outcome
-                fluent = causality_outcome.fluent
+    #     if causaulties:
+    #         for causality_outcome in causaulties.outcomes:
+    #             outcome = causality_outcome.outcome
+    #             fluent = copy.deepcopy(causality_outcome.fluent)
 
-                if outcome == A_TERMINATE:
-                    if KB.remove_fluent(fluent):
-                        KB.log_fluent(fluent, action.end_time, F_TERMINATE)
-                elif outcome == A_INITIATE:
-                    if KB.add_fluent(fluent):
-                        KB.log_fluent(fluent, action.end_time, F_INITIATE)
-                else:
-                    raise UnknownOutcomeError(outcome)
+    #             fluent.args = reify_args(fluent.args, action.args)
 
-    for fluent_outcome in chosen_solution[0].fluents:
-        if not CONFIG.cycle_fluents:
-            continue
+    #             if outcome == A_TERMINATE:
+    #                 if KB.remove_fluent(fluent):
+    #                     KB.log_fluent(fluent, action.end_time, F_TERMINATE)
+    #             elif outcome == A_INITIATE:
+    #                 if KB.add_fluent(fluent):
+    #                     KB.log_fluent(fluent, action.end_time, F_INITIATE)
+    #             else:
+    #                 raise UnknownOutcomeError(outcome)
 
-        outcome, fluent = fluent_outcome.outcome, fluent_outcome.fluent
+    # for fluent_outcome in chosen_solution[0].fluents:
+    #     if not CONFIG.cycle_fluents:
+    #         continue
 
-        if outcome == A_TERMINATE:
-            if KB.remove_fluent(fluent):
-                KB.log_fluent(fluent, cycle_time + 1, F_TERMINATE)
-        elif outcome == A_INITIATE:
-            if KB.add_fluent(fluent):
-                KB.log_fluent(fluent, cycle_time + 1, F_INITIATE)
-        else:
-            raise UnknownOutcomeError(outcome)
+    #     outcome, fluent = fluent_outcome.outcome, fluent_outcome.fluent
+
+    #     if outcome == A_TERMINATE:
+    #         if KB.remove_fluent(fluent):
+    #             KB.log_fluent(fluent, cycle_time + 1, F_TERMINATE)
+    #     elif outcome == A_INITIATE:
+    #         if KB.add_fluent(fluent):
+    #             KB.log_fluent(fluent, cycle_time + 1, F_INITIATE)
+    #     else:
+    #         raise UnknownOutcomeError(outcome)
 
     new_kb_goals = OrderedSet()
     for state, start_state in zip(chosen_solution[1], KB.goals):
 
         if state.result is G_SOLVED:
-            continue
+            _process_state(state)
         elif state.result is G_DEFER:
+            _process_state(state)
+
             new_state = copy.deepcopy(state)
 
             # Clear actions / fluents and set to unprocessed
@@ -86,3 +90,45 @@ def process_solutions(solutions, cycle_time):
     # print(new_kb_goals)
 
     KB.set_goals(new_kb_goals)
+
+
+def _process_state(state):
+
+    for action in state.actions:
+        KB.log_action_new(action)
+
+        if CONFIG.cycle_fluents:
+            continue
+
+        causaulties = KB.exists_causality(action)
+
+        if causaulties:
+            for causality_outcome in causaulties.outcomes:
+                outcome = causality_outcome.outcome
+                fluent = copy.deepcopy(causality_outcome.fluent)
+
+                fluent.args = reify_args(fluent.args, state.subs)
+
+                if outcome == A_TERMINATE:
+                    if KB.remove_fluent(fluent):
+                        KB.log_fluent(fluent, action.end_time, F_TERMINATE)
+                elif outcome == A_INITIATE:
+                    if KB.add_fluent(fluent):
+                        KB.log_fluent(fluent, action.end_time, F_INITIATE)
+                else:
+                    raise UnknownOutcomeError(outcome)
+
+    for fluent_outcome in state.fluents:
+        if not CONFIG.cycle_fluents:
+            continue
+
+        outcome, fluent = fluent_outcome.outcome, fluent_outcome.fluent
+
+        if outcome == A_TERMINATE:
+            if KB.remove_fluent(fluent):
+                KB.log_fluent(fluent, cycle_time + 1, F_TERMINATE)
+        elif outcome == A_INITIATE:
+            if KB.add_fluent(fluent):
+                KB.log_fluent(fluent, cycle_time + 1, F_INITIATE)
+        else:
+            raise UnknownOutcomeError(outcome)
