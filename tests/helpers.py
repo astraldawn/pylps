78dcf9f591ecaf044a31e97d24f7049ad7158f12
Mark <mark.lee.chu.yong@gmail.com>
2018-04-18 16:42:31 +0100

    circleci
    Update config.yml
    
    Update config.yml
    
    Update config.yml
    
    Update config.yml
    
    fix circleci path error
    
    Update config.yml
    
    Update config.yml
    
    fix char ending
    
    test_env

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,26 +1,44 @@
 import subprocess
+import os
+
+TEST_ENV = os.getenv('TEST_ENV', None)
+CIRCLE_CI_PATH = '/home/circleci/repo/'
+LINUX_SPLIT_CHAR = '\n'
+WINDOWS_SPLIT_CHAR = '\r\n'
 
 
 def run_pylps_example(example_name):
+    path = ''
+    split_char = WINDOWS_SPLIT_CHAR
+    if TEST_ENV:
+        path = CIRCLE_CI_PATH
+        split_char = LINUX_SPLIT_CHAR
+
     completed = subprocess.run(
-        ['python', 'examples/' + example_name + '.py'],
+        ['python', path + 'examples/' + example_name + '.py'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
 
-    actual = completed.stdout.decode('utf-8').split('\r\n')[:-1]
+    actual = completed.stdout.decode('utf-8').split(split_char)[:-1]
 
     return actual
 
 
 def run_pylps_test_program(program_group, program_name):
+    path = ''
+    split_char = WINDOWS_SPLIT_CHAR
+    if TEST_ENV:
+        path = CIRCLE_CI_PATH
+        split_char = LINUX_SPLIT_CHAR
+
     completed = subprocess.run(
-        ['python', 'tests/programs/' + program_group + '/' +
+        ['python', path + 'tests/programs/' + program_group + '/' +
          program_name + '.py'],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
 
-    actual = completed.stdout.decode('utf-8').split('\r\n')[:-1]
+    actual = completed.stdout.decode('utf-8').split(split_char)[:-1]
 
     return actual
