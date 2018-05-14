3c1de0c3e8435d46d21df185a7d2bf973b723854
Mark <mark.lee.chu.yong@gmail.com>
2018-05-14 20:47:50 +0100

    add scrolling for visualisation

--- pylps/visualiser.py
+++ pylps/visualiser.py
@@ -3,7 +3,8 @@ import copy
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import *
 kivy.require('1.0.7')
 
@@ -32,8 +33,9 @@ class TimePoint(object):
 
     def __repr__(self):
         ret = "Time %s\n" % str(self.time)
-        for event in self.events:
-            ret += "%s\n" % str(event)
+        for i in range(1):
+            for event in self.events:
+                ret += "%s\n" % str(event)
         ret += '\n'
         return ret
 
@@ -41,7 +43,21 @@ class TimePoint(object):
         self.events.append(event)
 
 
-class PylpsVisualiser(FloatLayout):
+class PylpsScreenManager(ScreenManager):
+    pass
+
+
+class TimePointDisplay(BoxLayout):
+    info = StringProperty()
+    identity = StringProperty()
+
+    def __init__(self, timepoint):
+        super().__init__()
+        self.identity = 'time' + str(timepoint.time)
+        self.info = str(timepoint)
+
+
+class PylpsMainScreen(Screen):
     info = StringProperty()
     current_time_str = StringProperty()
     max_time_str = StringProperty()
@@ -53,14 +69,21 @@ class PylpsVisualiser(FloatLayout):
         self.current_time = 0
         self.max_time = display_log[-1].end_time
         self.manual = False
+        self.tp_display_widgets = {}
+        self.widget_pos = {}
 
         # Generate data
         self.generate_timepoints()
 
-        for item in self.timepoints:
-            print(item)
+        for timepoint in self.timepoints:
+            self.add_timepoint_display(timepoint)
 
         self.update_display()
+
+    def add_timepoint_display(self, timepoint):
+        widget = TimePointDisplay(timepoint)
+        self.tp_display_widgets[timepoint.time] = widget
+        self.ids.scrollgrid.add_widget(widget)
 
     def generate_timepoints(self):
         self.timepoints = [TimePoint(i) for i in range(self.max_time + 1)]
@@ -68,7 +91,7 @@ class PylpsVisualiser(FloatLayout):
         for item in self.display_log:
             self.timepoints[item.end_time].add_event(item)
 
-    def update_display(self):
+    def update_display(self, scroll=True):
         # Bound checking
         if self.current_time > self.max_time:
             self.current_time = self.max_time
@@ -78,7 +101,11 @@ class PylpsVisualiser(FloatLayout):
 
         self.current_time_str = str(self.current_time)
         self.max_time_str = str(self.max_time)
-        self.info = str(self.timepoints[self.current_time])
+
+        if scroll:
+            self.ids.scrollgridview.scroll_to(
+                self.tp_display_widgets[self.current_time]
+            )
 
     def move_timepoint(self, command):
         commands = {
@@ -102,6 +129,13 @@ class PylpsVisualiser(FloatLayout):
         self.manual = False
         self.move_auto(first=True)
 
+    def scroll_view(self):
+        self.manual = True
+        scroll_pos = 1.1 - self.ids.scrollgridview.scroll_y
+        scroll_time = scroll_pos / (1 / self.max_time)
+        self.current_time = int(scroll_time)
+        self.update_display(scroll=False)
+
 
 class PylpsVisualiserApp(App):
 
@@ -110,4 +144,4 @@ class PylpsVisualiserApp(App):
         self.display_log = [LogObject(i) for i in display_log]
 
     def build(self):
-        return PylpsVisualiser(self.display_log)
+        return PylpsMainScreen(self.display_log)
